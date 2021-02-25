9c6c40e5a417572067248fdc24042cdeba7e7e63
zzheng07 <zzheng07@calpoly.edu>
2021-02-25 09:30:14 -0800

    added first test case

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('My first test case', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/My first Apollo app/i);
   expect(linkElement).toBeInTheDocument();
 });
