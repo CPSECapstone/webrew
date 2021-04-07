2c6dfe5d34571426af049f67160e6e6189d8dae0
Ryan Fay <rmfay@calpoly.edu>
2021-04-06 21:04:57 -0700

    added single student overview

--- src/index.tsx
+++ src/index.tsx
@@ -11,8 +11,7 @@ import reportWebVitals from './reportWebVitals';
 ReactDOM.render(
    <React.StrictMode>
       <App />
-   </React.StrictMode>,
-   document.getElementById('root')
+   </React.StrictMode>,document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
