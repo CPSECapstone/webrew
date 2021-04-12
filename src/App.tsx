0a59079765840117426841a129e5a23e543cabe6
mattcuento <cuentomr17@gmail.com>
2021-04-12 11:58:29 -0700

    created new router

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
-import React from 'react';
-import { ApolloProvider } from '@apollo/client';
-import apolloClient from './clients/apollo-client';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
-import NavigationRouter from './navigation/navigation-router';
+import CreateCourseDialog from './course/create-course-dialog';
+import Dashboard from './scenes/teacher/Dashboard';
+import StudentOverview from './student-overview/student-overview';
+import TaskOverview from './task-overview/task-overview';
+import TaskSubmission from './view-task-submission/view-task-submission';
+import Navigation from './navigation/Navigation';
 
 function App() {
    return (
-      <ApolloProvider client={apolloClient}>
-         <NavigationRouter />
-      </ApolloProvider>
+      <div className="App">
+         <Navigation />
+         <Switch>
+            <Route exact path="/" component={Dashboard} />
+            <Route path="/addNewCourse" component={CreateCourseDialog} />
+            <Route path="/addTaskSubmission" component={TaskSubmission} />
+            <Route path="/taskOverview" component={TaskOverview} />
+            <Route path="/studentOverview" component={StudentOverview} />
+         </Switch>
+      </div>
    );
 }
 
