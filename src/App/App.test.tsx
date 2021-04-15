4e07cd4fb412103048218e4e2e25786f865c28d8
Darian Nguyen <17971850+nguyen-darianhuy@users.noreply.github.com>
2021-04-14 18:03:59 -0700

    Remove default App.test

--- src/App/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('My first test case', () => {
-   render(<App />);
-   const linkElement = screen.getByText(/My first Apollo app/i);
-   expect(linkElement).toBeInTheDocument();
-});
