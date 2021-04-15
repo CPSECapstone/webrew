fdf83f72ba21e7dd15b88c44f2edaeed8879083e
mattcuento <cuentomr17@gmail.com>
2021-04-15 10:38:35 -0700

    Added navigation unit test

--- src/navigation/Navigation.tsx
+++ src/navigation/Navigation.tsx
@@ -3,12 +3,14 @@ import { Navbar, Nav, Container } from 'react-bootstrap';
 
 export default function Navigation() {
    return (
-      <Navbar collapseOnSelect expand="sm" bg="dark" variant="dark">
+      <Navbar collapseOnSelect expand="sm" bg="dark" variant="dark" data-testid="navbar-test">
          <Container>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
                <Nav>
-                  <Nav.Link href="/">Dashboard</Nav.Link>
+                  <Nav.Link href="/" data-testid="nav-link-first">
+                     Dashboard
+                  </Nav.Link>
                   <Nav.Link href="/addNewCourse">Create Course</Nav.Link>
                   <Nav.Link href="/addTaskSubmission">Submit Task</Nav.Link>
                   <Nav.Link href="/taskOverview">Tasks</Nav.Link>
---
+++ src/tests/navigation.test.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { render, fireEvent, waitFor, screen } from './test-utils';
+import '@testing-library/jest-dom/extend-expect';
+
+import Navigation from '../navigation/Navigation';
+
+test('displays modal', async () => {
+   // Arrange
+   render(<Navigation />);
+
+   // Act
+   await waitFor(() => screen.getByTestId('navbar-test'));
+
+   // Assert
+   expect(screen.getByTestId('nav-link-first')).toBeVisible();
+});
