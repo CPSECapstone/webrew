3a28a29a71627fe52776ef1e7bb379c0c586293c
zzheng07 <zzheng07@calpoly.edu>
2021-04-27 14:26:04 -0700

    restored learning objectives

--- src/Components/ViewTaskSubmission/ViewTaskSubmission.tsx
+++ src/Components/ViewTaskSubmission/ViewTaskSubmission.tsx
@@ -3,10 +3,13 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import { Radio } from '@material-ui/core';
 import { useQuery } from '@apollo/client';
 import { GET_LEARNING_OBJECTIVE } from '../../queries/LearningObjectiveQueries';
-import { LearningObjective } from '../../interfaces/LearningObjective';
 import { LearningObjectives } from '../../interfaces/LearningObjectives';
 import { Question, Option, QuizBlockData } from '../../interfaces/QuizBlock';
-import { QuizBlockSubmissionData } from '../../interfaces/QuizBlockSubmission';
+import {
+   QuizBlockSubmission,
+   QuizBlockSubmissionData,
+   StudentAnswer,
+} from '../../interfaces/QuizBlockSubmission';
 import { GET_QUIZBLOCK } from '../../queries/quizblock';
 import { GET_QUIZBLOCK_SUBMISSION } from '../../queries/quizblockSubmission';
 import './ViewTaskSubmission.css';
@@ -16,13 +19,11 @@ function ViewTaskSubmission() {
    const { data: quizblockSubmissionData } = useQuery<QuizBlockSubmissionData>(
       GET_QUIZBLOCK_SUBMISSION
    );
-   // const { data: learningObjectives } = useQuery<LearningObjectives>(GET_LEARNING_OBJECTIVE);
+   const { data: learningObjectives } = useQuery<LearningObjectives>(GET_LEARNING_OBJECTIVE);
 
-   // if (!learningObjectives) {
-   //    return <>Learning Objective Undefined</>;
-   // }
-   console.log(quizblockData);
-   console.log(quizblockSubmissionData);
+   if (!learningObjectives) {
+      return <>Learning Objective Undefined</>;
+   }
    if (!quizblockData) {
       return <p>quizblock undefined</p>;
    }
@@ -39,33 +40,45 @@ function ViewTaskSubmission() {
       ));
    }
 
-   function renderQuestionOptions(question: Question, answer: boolean) {
-      return question.options.map((option: Option) => {
+   function renderQuestionOptions(question: Question, answer: StudentAnswer) {
+      return question.options.map((option: Option, index: number) => {
          return (
             <FormControlLabel
                value={option.description}
                disabled
-               control={<Radio checked={answer} />}
+               control={<Radio checked={answer.choices[0] === index} />}
                label={option.description}
             />
          );
       });
    }
 
-   function renderFeedback(question: Question, answer: boolean) {
-      const feedback = answer
+   function renderFeedback(question: Question, answer: StudentAnswer) {
+      const feedback = answer.result
          ? `That's right! ${question.feedback}`
          : `Not quite. ${question.feedback}`;
       return feedback;
    }
 
-   function renderQuestions(questions: Question[]) {
+   function renderQuestions(questions: Question[], submission: QuizBlockSubmission) {
+      const answerMap = new Map<string, StudentAnswer>();
+      submission.studentAnswers.forEach((answer) => {
+         answerMap.set(answer.questionId, answer);
+      });
+
+      const defaulAnswer: StudentAnswer = {
+         questionId: 'question',
+         result: false,
+         choices: [0],
+      };
+
       return questions.map((question) => {
+         const answer = answerMap.get(question.id) || defaulAnswer;
          return (
             <div className="question">
                <div className="question-desc">{question.description}</div>
-               <RadioGroup>{renderQuestionOptions(question, true)}</RadioGroup>
-               <div className="feedback">{renderFeedback(question, true)}</div>
+               <RadioGroup>{renderQuestionOptions(question, answer)}</RadioGroup>
+               <div className="feedback">{renderFeedback(question, answer)}</div>
             </div>
          );
       });
@@ -73,12 +86,12 @@ function ViewTaskSubmission() {
 
    return (
       <div className="view-task-submission">
-         {/* <div className="objectives">{renderLearningObjectives(learningObjectives)}</div> */}
+         <div className="objectives">{renderLearningObjectives(learningObjectives)}</div>
          <div className="quizblock">
             <div>{quizblock.title}</div>
             <div>Total Points: {quizblock.points}</div>
             <div>Required Score: {quizblock.requiredScore}</div>
-            {renderQuestions(quizblock.questions)}
+            {renderQuestions(quizblock.questions, quizblockSubmission)}
          </div>
       </div>
    );
