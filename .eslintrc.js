2ef6267d736460cfab03aceba2568f7dc43b4178
zzheng07 <zzheng07@calpoly.edu>
2021-04-27 09:19:53 -0700

    formatted code and added configuration for debugger

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,26 @@
 module.exports = {
-    extends: [
+   extends: [
       'airbnb-typescript-prettier',
       'airbnb/hooks',
       'plugin:@typescript-eslint/recommended',
-      'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    ],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        project: './tsconfig.json',
-    },
-    plugins: ['prettier'],
-    rules: {
-        "import/prefer-default-export": "off",
-        "@typescript-eslint/explicit-module-boundary-types": "off",
-        "react/jsx-props-no-spreading": "off",
-        "react/react-in-jsx-scope": "off",
-        'prettier/prettier': 1,
-        "prettier/prettier": ["error", {
-            "endOfLine":"auto"
-          }],
-        "no-console": "off"
-    }
-  };
+      'plugin:@typescript-eslint/recommended-requiring-type-checking'
+   ],
+   parser: '@typescript-eslint/parser',
+   parserOptions: {
+      project: './tsconfig.json'
+   },
+   plugins: ['prettier'],
+   rules: {
+      'import/prefer-default-export': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      'react/jsx-props-no-spreading': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'prettier/prettier': [
+         'error',
+         {
+            endOfLine: 'auto'
+         }
+      ],
+      'no-console': 'off'
+   }
+};
