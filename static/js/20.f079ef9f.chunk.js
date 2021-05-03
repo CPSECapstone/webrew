982c84b3e40c64047847e007ebd18c2ada04e8a5
Shaurya Mago <shaurya@Shauryas-MBP.hsd1.ca.comcast.net>
2021-05-03 15:48:32 -0700

    Updates

---
+++ static/js/20.f079ef9f.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact-flipted"]=this["webpackJsonpreact-flipted"]||[]).push([[20],{585:function(t,e,n){"use strict";n.r(e),n.d(e,"amplify_container",(function(){return c}));var i=n(39),c=function(){function t(t){Object(i.k)(this,t)}return t.prototype.render=function(){return Object(i.i)(i.b,null,Object(i.i)("slot",null))},t}();c.style=":host{display:-ms-flexbox;display:flex;-ms-flex-align:center;align-items:center;-ms-flex-pack:center;justify-content:center;height:100vh}"}}]);
+//# sourceMappingURL=20.f079ef9f.chunk.js.map
