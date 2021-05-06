9404e0d5a5a8c36cd5aeb561ecbc9e53be4a8bbb
Shaurya Mago <shaurya@Shauryas-MBP.hsd1.ca.comcast.net>
2021-05-06 09:42:10 -0700

    heroku test

--- src/index.tsx
+++ src/index.tsx
@@ -17,8 +17,8 @@ Amplify.configure({
       oauth: {
          domain: 'flipted-ios-test.auth.us-east-1.amazoncognito.com',
          scope: ['phone', 'email', 'profile', 'openid', 'aws.cognito.signin.user.admin'],
-         redirectSignIn: 'https://webrew.herokuapp.com',
-         redirectSignOut: 'http://webrew.herokuapp.com',
+         redirectSignIn: 'https://webrew.herokuapp.com/',
+         redirectSignOut: 'https://webrew.herokuapp.com/',
          responseType: 'token',
       },
    },
