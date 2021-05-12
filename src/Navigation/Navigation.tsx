3a60f23066bb74330a8072cebbb6db20791aebc2
Shaurya Mago <shaurya@Shauryas-MBP.hsd1.ca.comcast.net>
2021-05-12 13:52:28 -0700

    fix styling

--- src/Navigation/Navigation.tsx
+++ src/Navigation/Navigation.tsx
@@ -1,20 +1,19 @@
-import { Auth } from 'aws-amplify';
-import { Navbar, Nav, Form, Button } from 'react-bootstrap';
+import { Navbar, Nav, Button, Form } from 'react-bootstrap';
+import { Auth, Hub } from 'aws-amplify';
 import './Navigation.css';
 
 export default function Navigation() {
    return (
-      <Navbar collapseOnSelect expand="sm" bg="primary" variant="dark">
+      <Navbar collapseOnSelect expand="sm" className="nav" variant="dark">
          <Navbar.Brand href="/">flipt.ED</Navbar.Brand>
          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
          <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="container-fluid">
-               <Nav.Link href="/">Dashboard</Nav.Link>
-               <Form inline>
-                  <Button className="ml-auto" type="submit" onClick={() => Auth.signOut()}>
+               <Nav.Item className="ml-auto">
+                  <Button variant="outline" type="submit" onClick={() => Auth.signOut()}>
                      Log Out
                   </Button>
-               </Form>
+               </Nav.Item>
             </Nav>
          </Navbar.Collapse>
       </Navbar>
