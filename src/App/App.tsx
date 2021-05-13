1393e23812c91c4ca400fb7cb6f3291e9680c411
Shaurya Mago <shaurya@Shauryas-MBP.hsd1.ca.comcast.net>
2021-05-13 14:03:01 -0700

    material-ui sidebar

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,14 +1,19 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import { Auth, Hub } from 'aws-amplify';
 import { useState, useEffect } from 'react';
 import { withAuthenticator } from 'aws-amplify-react';
 import Sidebar from '../Components/Sidebar';
 import Content from '../Components/Content';
 import Navigation from '../Navigation/Navigation';
+import StudentPicture from '../assets/images/images-1.png';
+
 import './App.scss';
 
 // Entry point of the Flitped App
 function App() {
    const [, setUser] = useState(null);
+   const [username, setUsername] = useState('');
+   const [fname, setFirstName] = useState('');
 
    function storeToken(): void {
       Auth.currentSession()
@@ -22,10 +27,11 @@ function App() {
          })
          .then((authUser) => {
             setUser(authUser);
+            setUsername(authUser.username);
+            setFirstName(authUser.attributes.given_name);
          })
          .catch(() => console.log('Not signed in'));
    }
-
    useEffect(() => {
       Hub.listen('auth', ({ payload: { event, data } }) => {
          switch (event) {
@@ -54,6 +60,8 @@ function App() {
          <div className="main container-fluid">
             <div className="row h-100">
                <div className="sidebar-container col-md-2">
+                  <img src={StudentPicture} alt="" style={{ width: 180, height: 180 }} />
+                  {fname}
                   <Sidebar />
                </div>
                <div className="content-container col-md-10">
