7b8c56882be820b015f48f79b031d15f1833fa06
Darian Nguyen <17971850+nguyen-darianhuy@users.noreply.github.com>
2021-05-13 20:56:03 -0700

    ClassMastery: Move mock data to clients/mock.ts

--- src/Screens/ClassMastery/ClassMastery.tsx
+++ src/Screens/ClassMastery/ClassMastery.tsx
@@ -4,6 +4,8 @@ import { color } from '../../Components/constants.json';
 
 import StudentMasteryRow, { StudentMasteryHeader } from './StudentMasteryRow';
 
+import { MOCK_LOBJ_NAMES, MOCK_LOBJ_MASTERY } from '../../clients/mock';
+
 const Page = styled.div`
    margin: 33px 90px;
 `;
@@ -16,50 +18,6 @@ const Header = styled.div`
 
 const Content = styled.div``;
 
-const MOCK_LOBJ_NAMES = ['Objective 1', 'Objective 2'];
-
-const MOCK_DATA = [
-   {
-      name: 'Ryan Fay',
-      learningObjectives: [
-         {
-            objectiveName: 'Objective 1',
-            percentage: 1,
-         },
-         {
-            objectiveName: 'Objective 2',
-            percentage: 0.9,
-         },
-      ],
-   },
-   {
-      name: 'Robert Middleton',
-      learningObjectives: [
-         {
-            objectiveName: 'Objective 1',
-            percentage: 0.6,
-         },
-         {
-            objectiveName: 'Objective 2',
-            percentage: 0.7,
-         },
-      ],
-   },
-   {
-      name: 'Darian Nguyen',
-      learningObjectives: [
-         {
-            objectiveName: 'Objective 1',
-            percentage: 0.24,
-         },
-         {
-            objectiveName: 'Objective 2',
-            percentage: 0.14,
-         },
-      ],
-   },
-];
-
 function ClassMastery() {
    //TODO Get real data
    return (
@@ -67,7 +25,7 @@ function ClassMastery() {
          <Header>Class Mastery</Header>
          <Content>
             <StudentMasteryHeader learningObjectiveNames={MOCK_LOBJ_NAMES} />
-            {MOCK_DATA.map(({ name, learningObjectives }) => (
+            {MOCK_LOBJ_MASTERY.map(({ name, learningObjectives }) => (
                <StudentMasteryRow name={name} learningObjectives={learningObjectives} />
             ))}
          </Content>
