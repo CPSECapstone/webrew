020027ff2d4015f243c02028a16669bec3870a63
Darian Nguyen <17971850+nguyen-darianhuy@users.noreply.github.com>
2021-06-01 12:35:27 -0700

    Added test from 54b211d

---
+++ src/tests/single-student-mastery-overview.test.tsx
@@ -0,0 +1,17 @@
+import { fireEvent, render, screen, waitFor } from './test-utils';
+import '@testing-library/jest-dom/extend-expect';
+// import CreateCourseDialog from '../course/create-course-dialog';
+import SingleStudentMasteryOverview from '../single-student-mastery-overview/single-student-mastery-overview';
+
+test('displays student overview', async () => {
+   // Arrange
+   render(<SingleStudentMasteryOverview />);
+
+   // Act
+   fireEvent.click(screen.getByText('Target 1'));
+
+   await waitFor(() => screen.getByTestId('mastery-overview'));
+
+   // Assert
+   expect(screen.getAllByLabelText('all-objectives')).toHaveLength(4);
+});
