63644229dbdf1c029b298828856ee895ac939752
Destin Estrela <destin.estrela@unity3d.com>
2021-06-29 12:48:55 -0700

    Removed faulty test

--- src/Components/MarketHome/StudentsTab.tsx
+++ src/Components/MarketHome/StudentsTab.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unsafe-return */
 import { CircularProgress } from '@material-ui/core';
-import styled from 'styled-components';
 
 import { useMemo } from 'react';
 import { useParams } from 'react-router-dom';
