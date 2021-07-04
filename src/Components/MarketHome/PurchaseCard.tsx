c6216b9b0a225603a63323b9411f8fe4cc19a626
Destin Estrela <destin.estrela@gmail.com>
2021-07-04 13:16:40 -0700

    Refund functional

--- src/Components/MarketHome/PurchaseCard.tsx
+++ src/Components/MarketHome/PurchaseCard.tsx
@@ -6,6 +6,7 @@ import {
    FulfillMutation,
    ReceiptInfoFragment,
    useFulfillMutation,
+   useRefundMutation,
 } from '../../__generated__/types';
 import ReceiptMenu from './SimplyMenu';
 
@@ -14,6 +15,7 @@ type Props = {
    displayStudentName: boolean;
    displayListingName: boolean;
    editPurchase: any;
+   deletePurchase: any;
 };
 
 function determineMiddleText(displayListingName: boolean, displayStudentName: boolean) {
@@ -31,21 +33,36 @@ export function PurchaseCard({
    displayListingName,
    displayStudentName,
    editPurchase,
+   deletePurchase,
 }: Props) {
    const middleText = determineMiddleText(displayListingName, displayStudentName);
 
-   const onMutationCompleted = (data: FulfillMutation) => {
+   const onFulfillCompleted = (data: FulfillMutation) => {
       console.log('Mutation Completed');
       editPurchase(data.fulfillPurchase);
    };
 
-   const onMutationError = (e: ApolloError) => {
+   const onRefundCompleted = () => {
+      console.log('Mutation Completed');
+      deletePurchase(purchase.receiptId);
+   };
+
+   const onFulfillError = (e: ApolloError) => {
       console.log(e.message);
    };
 
-   const [fulfill, { loading: mutationLoading }] = useFulfillMutation({
-      onCompleted: onMutationCompleted,
-      onError: onMutationError,
+   const onRefundError = (e: ApolloError) => {
+      console.log(e.message);
+   };
+
+   const [fulfill, { loading: fulfillLoading }] = useFulfillMutation({
+      onCompleted: onFulfillCompleted,
+      onError: onFulfillError,
+   });
+
+   const [refund] = useRefundMutation({
+      onCompleted: onRefundCompleted,
+      onError: onRefundError,
    });
 
    const onClickFulfill = () => {
@@ -68,9 +85,22 @@ export function PurchaseCard({
       }).catch((e) => console.log(e));
    };
 
+   const onClickRefund = () => {
+      refund({
+         variables: {
+            course: purchase.course,
+            receiptId: purchase.receiptId,
+         },
+      }).catch((e) => console.log(e));
+   };
+
    return (
       <div className="purchase-card">
-         <ReceiptMenu onClickUnfulfill={onClickUnFulfill} fulfilled={purchase.fulfilled} />
+         <ReceiptMenu
+            onClickUnfulfill={onClickUnFulfill}
+            fulfilled={purchase.fulfilled}
+            onClickRefund={onClickRefund}
+         />
          {!purchase.fulfilled ? (
             <div>
                <Button
@@ -86,7 +116,7 @@ export function PurchaseCard({
                   onClick={onClickFulfill}
                >
                   <span>Fulfill </span>
-                  {mutationLoading ? <CircularProgress size={15} /> : <></>}
+                  {fulfillLoading ? <CircularProgress size={15} /> : <></>}
                </Button>
             </div>
          ) : (
