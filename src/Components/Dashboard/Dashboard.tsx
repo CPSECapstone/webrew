636862c8f23a8d07d8d4238c71aac40a523aab01
Destin Estrela <destin.estrela@gmail.com>
2021-07-26 18:48:52 -0500

    Students can join courses

--- src/Components/Dashboard/Dashboard.tsx
+++ src/Components/Dashboard/Dashboard.tsx
@@ -2,6 +2,7 @@ import { CourseInfoFieldsFragment, Role } from '../../__generated__/types';
 import CourseCard from './CourseCard';
 import CreateCourseDialog from '../CreateCourseDialog/CreateCourseDialog';
 import './DashBoard.css';
+import { JoinCourseDialog } from '../MarketHome/Student/JoinCourseDialog';
 
 type Props = {
    courses: CourseInfoFieldsFragment[];
@@ -12,7 +13,11 @@ type Props = {
 function Dashboard({ courses, refetchCourses, role }: Props) {
    return (
       <div>
-         {role === Role.Instructor ? <CreateCourseDialog refetch={refetchCourses} /> : <></>}
+         {role === Role.Instructor ? (
+            <CreateCourseDialog refetch={refetchCourses} />
+         ) : (
+            <JoinCourseDialog refetch={refetchCourses} />
+         )}
          {courses.map((courseInfo: CourseInfoFieldsFragment) => (
             <CourseCard key={courseInfo.courseId} courseInfo={courseInfo} />
          ))}
---
+++ src/Components/MarketHome/Student/JoinCourseForm.tsx
@@ -0,0 +1,98 @@
+import { DialogActions } from '@material-ui/core';
+import { Button } from 'aws-amplify-react';
+import { Formik, Form } from 'formik';
+import { StudentInput } from '../../../__generated__/types';
+import { SmallTextField } from '../FieldStyles';
+
+type Props = {
+   initialValues: JoinCourseFormInput;
+   onSubmit: (values: JoinCourseFormInput) => void;
+};
+
+export type JoinCourseFormInput = {
+   firstName: string;
+   lastName: string;
+   courseId: string;
+   instructorId: string;
+};
+
+export function joinCourseFormToInputType(input: JoinCourseFormInput): StudentInput {
+   return {
+      firstName: input.firstName,
+      lastName: input.lastName,
+      studentId: '',
+      instructorId: input.instructorId,
+      courseId: input.courseId,
+   };
+}
+
+export function JoinCourseForm({ initialValues, onSubmit }: Props) {
+   return (
+      <Formik
+         initialValues={initialValues}
+         onSubmit={(values, { setSubmitting }) => {
+            setSubmitting(false);
+            onSubmit(values);
+         }}
+      >
+         {({ values, handleChange, handleBlur, handleSubmit, isSubmitting }) => (
+            <Form onSubmit={handleSubmit}>
+               <SmallTextField
+                  required
+                  id="courseId"
+                  label="Course ID"
+                  type="text"
+                  fullWidth
+                  variant="outlined"
+                  margin="dense"
+                  value={values.courseId}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+               />
+               <SmallTextField
+                  required
+                  id="instructorId"
+                  label="Instructor ID"
+                  type="text"
+                  fullWidth
+                  variant="outlined"
+                  margin="dense"
+                  value={values.instructorId}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+               />
+
+               <SmallTextField
+                  required
+                  id="firstName"
+                  label="First Name"
+                  type="text"
+                  fullWidth
+                  variant="outlined"
+                  margin="dense"
+                  value={values.firstName}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+               />
+               <SmallTextField
+                  required
+                  id="lastName"
+                  label="Last Name"
+                  type="text"
+                  fullWidth
+                  variant="outlined"
+                  margin="dense"
+                  value={values.lastName}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+               />
+               <DialogActions>
+                  <Button type="submit" disabled={isSubmitting} color="primary">
+                     Add
+                  </Button>
+               </DialogActions>
+            </Form>
+         )}
+      </Formik>
+   );
+}
