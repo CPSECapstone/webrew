ca0d120ea9b35aa882a4ce82e23a49069359f315
Destin Estrela <destin.estrela@gmail.com>
2021-08-16 13:45:11 -0400

    Hid changed unused var warnings

--- src/Components/MarketHome/Student/JoinCourseLink.tsx
+++ src/Components/MarketHome/Student/JoinCourseLink.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { ApolloError } from '@apollo/client';
 import { useEffect } from 'react';
 import { useHistory, useParams } from 'react-router-dom';
